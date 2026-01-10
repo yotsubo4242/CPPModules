88d6c2182701f5e26adb549bb6ecbc2dddcc4a49
yotsubo <513047.otsubo@gmail.com>
2026-01-10 17:24:42 +0900

    mod: cpp01/ex01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -9,7 +9,14 @@ Zombie *zombieHorde(int, std::string);
 int	main(void) {
 	std::cout << "\033[33m Testing zombieHorde() function... \033[m" << std::endl;
 	Zombie *horde = zombieHorde(N, NAME);
-	std::cout << "\033[33m DONE: Testing zombieHorde() function!! \033[m" << std::endl;
+	if (horde == NULL)
+		return (1);
+	std::cout << "\033[33m DONE: Testing zombieHorde() function \033[m" << std::endl;
+
+	std::cout << "\033[33m Testing announce() each zombie \033[m" << std::endl;
+	for (int i = 0; i < N; i++)
+		horde[i].announce();
+	std::cout << "\033[33m DONE: Testing announce() function \033[m" << std::endl;
 
 	delete[] horde;
 	return (0);
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -3,12 +3,15 @@
 
 Zombie *zombieHorde(int N, std::string name) {
 	std::cout << "now, in the zombieHorde() function" << std::endl;
-	Zombie *res = new Zombie[N];
+	Zombie *res;
+	try {
+		res = new Zombie[N];
+	} catch (std::bad_alloc& e) {
+		std::cout << "ERROR: " << e.what() << std::endl;
+		return (NULL);
+	}
 	for (int i = 0; i < N; i++) {
-		std::cout << "\033[33m Testing setName() function... \033[m" << std::endl;
 		res[i].setName(name);
-		std::cout << "\033[33m Testing announce() function... \033[m" << std::endl;
-		res[i].announce();
 	}
 	return (res);
 }
